d34449f2e130e6358018edb335576d0906f89b3b
ryandavis <rcdavis13@gmail.com>
2022-12-07 22:04:25 -0500

    d2p1 fixes

--- day02/day02part01.py
+++ day02/day02part01.py
@@ -3,24 +3,24 @@ Y = 2
 Z = 3
 score = 0
 
-with open('input-2.txt') as input_file:
+with open('day02/input-2.txt') as input_file:
     for line in input_file:
         if line[0] == 'A' and line[2] == 'X':
-            score = score + 1 + 3
-        elif line[0] == 'A' and line[2] == 'Y':
-            score = score + 2 + 6
-        elif line[0] == 'A' and line[2] == 'Z':
             score = score + 3 + 0
+        elif line[0] == 'A' and line[2] == 'Y':
+            score = score + 1 + 3
+        elif line[0] == 'A' and line[2] == 'Z':
+            score = score + 2 + 6
         elif line[0] == 'B' and line[2] == 'X':
             score = score + 1 + 0
         elif line[0] == 'B' and line[2] == 'Y':
-            score = score + 2 + 3  
+            score = score + 2 + 3
         elif line[0] == 'B' and line[2] == 'Z':
             score = score + 3 + 6
         elif line[0] == 'C' and line[2] == 'X':
-            score = score + 1 + 6
-        elif line[0] == 'C' and line[2] == 'Y':
             score = score + 2 + 0
+        elif line[0] == 'C' and line[2] == 'Y':
+            score = score + 3 + 3
         elif line[0] == 'C' and line[2] == 'Z':
-            score = score + 3 + 0
+            score = score + 1 + 6
 print(score)
