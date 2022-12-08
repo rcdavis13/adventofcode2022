85959d91a8646863eb213e29444c060b8c824c8a
ryandavis <rcdavis13@gmail.com>
2022-12-07 21:48:26 -0500

    d2p1 fixes

--- day02/day02part01.py
+++ day02/day02part01.py
@@ -1,6 +1,3 @@
-A = 
-B = 
-C = 
 X = 1
 Y = 2
 Z = 3
@@ -8,22 +5,22 @@ score = 0
 
 with open('input-2.txt') as input_file:
     for line in input_file:
-        if line[0] == 'A' & line[2] == 'X':
+        if line[0] == 'A' and line[2] == 'X':
             score = score + 1 + 3
-        elif line[0] == 'A' & line[2] == 'Y':
+        elif line[0] == 'A' and line[2] == 'Y':
             score = score + 2 + 6
-        elif line[0] == 'A' & line[2] == 'Z':
+        elif line[0] == 'A' and line[2] == 'Z':
             score = score + 3 + 0
-        elif line[0] == 'B' & line[2] == 'X':
+        elif line[0] == 'B' and line[2] == 'X':
             score = score + 1 + 0
-        elif line[0] == 'B' & line[2] == 'Y':
+        elif line[0] == 'B' and line[2] == 'Y':
             score = score + 2 + 3  
-        elif line[0] == 'B' & line[2] == 'Z':
+        elif line[0] == 'B' and line[2] == 'Z':
             score = score + 3 + 6
-        elif line[0] == 'C' & line[2] == 'X':
+        elif line[0] == 'C' and line[2] == 'X':
             score = score + 1 + 6
-        elif line[0] == 'C' & line[2] == 'Y':
+        elif line[0] == 'C' and line[2] == 'Y':
             score = score + 2 + 0
-        elif line[0] == 'C' & line[2] == 'Z':
+        elif line[0] == 'C' and line[2] == 'Z':
             score = score + 3 + 0
 print(score)
